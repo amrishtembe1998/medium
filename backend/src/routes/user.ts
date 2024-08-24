3d0ca148213abc3ba9e9dcee472256816ecb1800
amrishtembe1998 <amrishtembe1998@gmail.com>
2024-08-24 19:37:39 +0530

    New route to show user info

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
+import { sign, verify, decode } from "hono/jwt";
 import {
   SignUpSchema,
   SignInSchema,
@@ -70,4 +70,31 @@ userRouter.post("/signin", async (c) => {
   }
   const signedJWT = await sign({ id: user.id }, c.env.JWT_SECRET);
   return c.json({ message: "Signed In successfully", jwtToken: signedJWT });
+});
+
+userRouter.get("/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const authorization = c.req.header("Authorization");
+  const token = authorization?.split(" ")[1];
+  try {
+    const isValid = await verify(token || "", c.env.JWT_SECRET);
+    if (!isValid) {
+      return c.json({ error: "Token is invalid" });
+    }
+  } catch (error) {
+    return c.json({ error: "Error in verifying access token" });
+  }
+  const decodedToken = decode(token || "");
+  const user = await prisma.user.findUnique({
+    where: {
+      id: decodedToken.payload.id as string,
+    },
+  });
+  if (!user) {
+    c.status(404);
+    return c.json({ error: "User not found" });
+  }
+  return c.json({ user });
 });
